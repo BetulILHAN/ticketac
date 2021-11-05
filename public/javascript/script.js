5a105dbd55a199452a65f2ab61170e91e913fc01
Valentine Lavaly <valentine.lavaly@outlook.fr>
2021-11-05 12:32:23 +0100

    pop up sur orders

---
+++ public/javascript/script.js
@@ -0,0 +1,7 @@
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+
+open.addEventListener('click',() => {
+modal_container.classList.add('show');
+});
+
